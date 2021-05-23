f741937a865a04403f8194d6166389ad421052db
WalukaUI <cwaluka@yahoo.com>
2021-05-23 11:10:09 -0700

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+import react, { useEffect, useState } from "react"
+
+function App(){
+  const [datas,setData]=useState([])
+      useEffect(()=>{
+          fetch("https://dog.ceo/api/breeds/image/random")
+          .then(res=>res.json())
+          .then(json=>setData(json.message))      
+      },[])
+      
+      return <p><img src={datas} /> </p>
+  }
+  
+  export default App
