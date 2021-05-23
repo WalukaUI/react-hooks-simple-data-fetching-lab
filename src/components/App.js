3a3be2bd2acbdafb97ccc0b4158f55ed080e76b4
WalukaUI <cwaluka@yahoo.com>
2021-05-23 12:05:48 -0700

    all

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@
 import react, { useEffect, useState } from "react"
 
 function App(){
-  const [datas,setData]=useState([])
+  const [apiData,setApiData]=useState([])
+  const [isLoaded,setIsLoaded]=(false)
       useEffect(()=>{
           fetch("https://dog.ceo/api/breeds/image/random")
           .then(res=>res.json())
-          .then(json=>setData(json.message))      
+          .then(json=>{setApiData(json.message)
+            setIsLoaded(true)})      
       },[])
-      
-      return <p><img src={datas} /> </p>
+      if(isLoaded===true) return <p>Loading....</p>
+      return <img alt="A Random Dog" src={apiData} />
   }
   
   export default App
